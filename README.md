b4b007cba9596051384070d68c4fbdedc8e21cf5
keddad <keddad@yandex.ru>
2022-03-28 16:21:33 +0300

    Simple CLI

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # gosuggest
-Simple suggestion engine written in Go as part of VK internship tests.
+Simple suggestion engine for films (among other things) written in Go as part of VK internship tests.
 
-* ```data.csv``` - 5900 film titles with ratings, generated with 
+* ```data.csv``` - 5900 film titles with ratings, generated with ```data_extractor.py``` from [Dataset](https://www.kaggle.com/datasets/rounakbanik/the-movies-dataset). Only movies with ratings and titles are present.
+* ```trie``` is a Golang module which contains a "Fuzzy Trie" - modified Trie data structure which supports finding a "close match" among words, with BFS-like traversal. To rank the matches, it uses length difference, Levenshtein distance and film's score. It has some unit tests.
+* ```main.go``` - simple CLI to showcase the functionality of the app.
--- main.go
+++ main.go
@@ -1,5 +1,59 @@
 package main
 
+import (
+	"bufio"
+	"bytes"
+	"encoding/csv"
+	"fmt"
+	trie "github.com/keddad/gosuggest/trie"
+	"log"
+	"os"
+	"strconv"
+)
+
+func getRealSizeOf(v interface{}) int {
+	b := new(bytes.Buffer)
+	return b.Len()
+}
+
+func readCsvFile(filePath string) [][]string {
+	// https://stackoverflow.com/questions/24999079/reading-csv-file-in-go
+	f, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal("Unable to read input file "+filePath, err)
+	}
+	defer f.Close()
+
+	csvReader := csv.NewReader(f)
+	records, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatal("Unable to parse file as CSV for "+filePath, err)
+	}
+
+	return records
+}
+
 func main() {
-	println("Hello, world.")
+	lines := readCsvFile("data.csv")
+	trie := trie.InitTrie()
+
+	for _, line := range lines {
+		score, _ := strconv.Atoi(line[2])
+		trie.Insert([]rune(line[1]), score)
+	}
+
+	fmt.Printf("Trie built. Size is: %d \n", trie.Words)
+	reader := bufio.NewReader(os.Stdin)
+
+	for true {
+		fmt.Print("Input search query:\n>")
+		text, _ := reader.ReadString('\n')
+
+		matches := trie.FindClosest([]rune(text[:len(text)-1]))
+
+		for i, match := range matches {
+			fmt.Printf("%d: %s\n", i, string(match))
+		}
+
+	}
 }
